7c9335ad0d1afdbea60165d22e3fad11f58d9811
Alvin Wanjohi <akwanjohi99@gmail.com>
2023-10-12 17:45:14 +0300

    Modify header component
    - Implement logout functionality and dynamic user information

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -66,9 +66,11 @@
                                     </div>
 
                                     <div class="d-flex flex-column">
-                                        <div class="fw-bold d-flex align-items-center fs-5">Max Smith
-                                        <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Super</span></div>
-                                        <a href="#" class="fw-semibold text-muted text-hover-primary fs-7">max@kt.com</a>
+                                        <div class="fw-bold d-flex align-items-center fs-5">
+                                            {{ authUser.full_name }}
+                                            <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Admin</span>
+                                        </div>
+                                        <p class="fw-semibold text-muted fs-7">{{ authUser.email }}</p>
                                     </div>
                                 </div>
                             </div>
@@ -76,34 +78,13 @@
                             <div class="separator my-2"></div>
 
                             <div class="menu-item px-5">
-                                <a href="#" class="menu-link px-5">My Profile</a>
-                            </div>
-
-                            <div class="menu-item px-5">
-                                <a href="#" class="menu-link px-5">
-                                    <span class="menu-text">My Projects</span>
-                                    <span class="menu-badge">
-                                        <span class="badge badge-light-danger badge-circle fw-bold fs-7">3</span>
-                                    </span>
-                                </a>
-                            </div>
-
-                            <div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0">
-                                <a href="#" class="menu-link px-5">
-                                    <span class="menu-title">My Subscription</span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <div class="menu-sub menu-sub-dropdown w-175px py-4">
-                                    <div class="menu-item px-3">
-                                        <a href="#" class="menu-link px-5">Referrals</a>
-                                    </div>
-                                </div>
+                                <span class="menu-link px-5">My Profile</span>
                             </div>
 
                             <div class="separator my-2"></div>
 
                             <div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0">
-                                <a href="#" class="menu-link px-5">
+                                <span class="menu-link px-5">
                                     <span class="menu-title position-relative">
                                         Mode
                                         <span class="ms-5 position-absolute translate-middle-y top-50 end-0">
@@ -111,37 +92,37 @@
                                             <i class="ki-outline ki-moon theme-dark-show fs-2"></i>
                                         </span>
                                     </span>
-                                </a>
+                                </span>
                                 <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-title-gray-700 menu-icon-gray-500 menu-active-bg menu-state-color fw-semibold py-4 fs-base w-150px" data-kt-menu="true" data-kt-element="theme-mode-menu">
                                     <div class="menu-item px-3 my-0">
-                                        <a href="#" class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="light">
+                                        <span class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="light">
                                             <span class="menu-icon" data-kt-element="icon">
                                                 <i class="ki-outline ki-night-day fs-2"></i>
                                             </span>
                                             <span class="menu-title">Light</span>
-                                        </a>
+                                        </span>
                                     </div>
                                     <div class="menu-item px-3 my-0">
-                                        <a href="#" class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="dark">
+                                        <span class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="dark">
                                             <span class="menu-icon" data-kt-element="icon">
                                                 <i class="ki-outline ki-moon fs-2"></i>
                                             </span>
                                             <span class="menu-title">Dark</span>
-                                        </a>
+                                        </span>
                                     </div>
                                     <div class="menu-item px-3 my-0">
-                                        <a href="#" class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="system">
+                                        <span class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="system">
                                             <span class="menu-icon" data-kt-element="icon">
                                                 <i class="ki-outline ki-screen fs-2"></i>
                                             </span>
                                             <span class="menu-title">System</span>
-                                        </a>
+                                        </span>
                                     </div>
                                 </div>
                             </div>
 
                             <div class="menu-item px-5">
-                                <a href="#" class="menu-link px-5" @click.prevent="">Sign Out</a>
+                                <span class="menu-link px-5" @click.prevent="signOut">Sign Out</span>
                             </div>
                         </div>
                     </div>
@@ -152,6 +133,23 @@
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '@/stores/authStore';
+import { toast } from "vue3-toastify";
+
+const router = useRouter()
+const { authUser, logout } = useAuthStore()
+
+const signOut = async () => {
+    let response = await logout()
+
+    if (response.status == 200) {
+        router.push({name: 'login'})
+    } else {
+        let err = response.data
+        toast.error(err.message)
+    }
+}
 
 </script>
 
