2992c5e1e223bd6f9e7dd7a46aa197b9c45cf401
Alvin Wanjohi <akwanjohi99@gmail.com>
2023-10-12 17:29:04 +0300

    Modify auth store
    - Implement login and logout actions

--- resources/js/stores/authStore.js
+++ resources/js/stores/authStore.js
@@ -5,8 +5,45 @@ export const useAuthStore = defineStore(
     "authStore",
     () => {
         const authUser = ref(null);
+        const token = ref(null);
 
-        return { authUser };
+        const login = async (data) => {
+            let response = null;
+            try {
+                response = await axios.post("login", data);
+            } catch (error) {
+                response = error.response;
+            }
+
+            if (response.status == 200) {
+                authUser.value = response.data.admin;
+                token.value = response.data.token;
+            }
+
+            return response;
+        };
+
+        const logout = async () => {
+            let response = null;
+            try {
+                response = await axios.delete("logout", {
+                    headers: {
+                        Authorization: `Bearer ${token.value}`,
+                    },
+                });
+            } catch (error) {
+                response = error.response;
+            }
+
+            if (response.status == 200) {
+                authUser.value = null;
+                token.value = null;
+            }
+
+            return response;
+        };
+
+        return { authUser, token, login, logout };
     },
     {
         persist: true,
