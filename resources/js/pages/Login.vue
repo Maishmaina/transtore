2600b8166ed27da3d7e22236b7c739cab633c7a9
Alvin Wanjohi <akwanjohi99@gmail.com>
2023-10-12 17:33:22 +0300

    Modify login component
    - Implement login functionality

--- resources/js/pages/Login.vue
+++ resources/js/pages/Login.vue
@@ -1,25 +1,31 @@
 <template>
     <Auth>
-        <form class="form w-100" @submit.prevent="">
+        <form class="form w-100" @submit.prevent="submitForm">
             <div class="text-center mb-11">
                 <h1 class="text-dark fw-bolder mb-3">Sign In</h1>
                 <div class="text-gray-500 fw-semibold fs-6">Sign In to access the application</div>
             </div>
 
             <div class="fv-row mb-8">
-                <input type="text" placeholder="Email" name="email" autocomplete="off" class="form-control bg-transparent" />
+                <input type="text" placeholder="Email" class="form-control bg-transparent" :class="{'is-invalid': errors.email}" v-model="form.email" />
+                <div class="invalid-feedback" v-if="errors.email">
+                    {{ errors.email[0] }}
+                </div>
             </div>
             <div class="fv-row mb-3">
-                <input type="password" placeholder="Password" name="password" autocomplete="off" class="form-control bg-transparent" />
+                <input type="password" placeholder="Password" class="form-control bg-transparent" :class="{'is-invalid': errors.password}" v-model="form.password" />
+                <div class="invalid-feedback" v-if="errors.password">
+                    {{ errors.password[0] }}
+                </div>
             </div>
             <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
                 <div></div>
                 <router-link :to="{name: 'forgot-password'}" class="link-primary">Forgot Password ?</router-link>
             </div>
             <div class="d-grid mb-10">
-                <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
-                    <span class="indicator-label">Sign In</span>
-                    <span class="indicator-progress">
+                <button type="submit" id="kt_sign_in_submit" class="btn btn-primary" :disabled="processing">
+                    <span class="indicator-label" v-if="!processing">Sign In</span>
+                    <span class="indicator-progress d-block" v-else>
                         Please wait...
                         <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
                     </span>
@@ -30,7 +36,45 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '@/stores/authStore';
 import Auth from '@/components/Auth.vue'
+import { toast } from "vue3-toastify";
+
+const router = useRouter()
+let { login } = useAuthStore()
+
+const form = ref({
+    email: '',
+    password: ''
+})
+
+const errors = ref({})
+
+const processing = ref(false)
+
+const submitForm = async () => {
+    errors.value = {}
+    
+    processing.value = true
+
+    let response = await login(form.value)
+
+    processing.value = false
+
+    if (response.status == 200) {
+        // router.push({name: 'home'})
+        window.location.href = '/'
+    } else {
+        let error = response.data
+        
+        toast.error(error.message)
+        errors.value = error.errors ?? {}
+    }
+
+}
+
 </script>
 
 <style scoped></style>
