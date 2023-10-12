33ffa3649853069935d131448afe0826d8f4d07c
Alvin Wanjohi <akwanjohi99@gmail.com>
2023-10-12 14:35:25 +0300

    Install vue3-toastify package
    - Configure it globally on app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,8 @@ import "./bootstrap";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import piniaPluginPersistedState from "pinia-plugin-persistedstate";
+import Vue3Toasity, { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
 
 import App from "./App.vue";
 import router from "./router";
@@ -12,5 +14,8 @@ pinia.use(piniaPluginPersistedState);
 
 app.use(router);
 app.use(pinia);
+app.use(Vue3Toasity, {
+    autoClose: 3000,
+});
 
 app.mount("#app");
