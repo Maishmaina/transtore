aadb574f1c38db0a693a4fc214fd259af00411ce
Alvin Wanjohi <akwanjohi99@gmail.com>
2023-10-12 17:35:07 +0300

    Modify reset-password  component
    - Implement functionality

--- resources/js/pages/ResetPassword.vue
+++ resources/js/pages/ResetPassword.vue
@@ -1,6 +1,6 @@
 <template>
     <Auth>
-        <form class="form w-100" @submit.prevent="">
+        <form class="form w-100" @submit.prevent="submitForm">
             <div class="text-center mb-10">
                 <h1 class="text-dark fw-bolder mb-3">Setup New Password</h1>
                 <div class="text-gray-500 fw-semibold fs-6">
@@ -10,18 +10,27 @@
             </div>
 
             <div class="fv-row mb-8">
-                <input type="text" placeholder="Reset Code" name="reset-code" autocomplete="off" class="form-control bg-transparent" />
+                <input type="text" placeholder="Reset Code" autocomplete="off" class="form-control bg-transparent" :class="{'is-invalid': errors.reset_code}" v-model="form.reset_code" />
+                <div class="invalid-feedback" v-if="errors.reset_code">
+                    {{ errors.reset_code[0] }}
+                </div>
             </div>
 
             <div class="fv-row mb-8">
-                <input type="password" placeholder="Password" name="password" autocomplete="off" class="form-control bg-transparent" />
+                <input type="password" placeholder="Password" autocomplete="off" class="form-control bg-transparent" :class="{'is-invalid': errors.password}" v-model="form.password" />
+                <div class="invalid-feedback" v-if="errors.password">
+                    {{ errors.password[0] }}
+                </div>
             </div>
 
             <div class="fv-row mb-8">
-                <input type="password" placeholder="Repeat Password" name="confirm-password" autocomplete="off" class="form-control bg-transparent" />
+                <input type="password" placeholder="Repeat Password" autocomplete="off" class="form-control bg-transparent" :class="{'is-invalid': errors.password_confirmation}" v-model="form.password_confirmation" />
+                <div class="invalid-feedback" v-if="errors.password_confirmation">
+                    {{ errors.password_confirmation[0] }}
+                </div>
             </div>
             <div class="d-grid mb-10">
-                <button type="button" id="kt_new_password_submit" class="btn btn-primary">
+                <button type="submit" id="kt_new_password_submit" class="btn btn-primary">
                     <span class="indicator-label">Submit</span>
                     <span class="indicator-progress">
                         Please wait...
@@ -34,7 +43,44 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router';
 import Auth from '@/components/Auth.vue'
+import { toast } from "vue3-toastify";
+
+const router = useRouter()
+
+const errors = ref({})
+const processing = ref(false)
+
+const form = ref({
+    reset_code: '',
+    password: '',
+    password_confirmation: ''
+})
+
+const submitForm = async () => {
+    errors.value = {}
+    processing.value = true
+
+    let response = null
+    try {
+        response = await axios.post('reset-password', form.value)
+    } catch (error) {
+        response = error.response
+        error = response.data
+
+        toast.error(error.message)
+        errors.value = error.errors ?? {}
+    }
+
+    processing.value = false
+
+    if (response.status == 200) {
+        router.push({name: 'login'})
+    }
+}
+
 </script>
 
 <style scoped></style>
