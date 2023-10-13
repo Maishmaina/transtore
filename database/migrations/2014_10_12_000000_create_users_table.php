d12544e61f648fd48b790dad3025a2a739b944d7
Alvin Wanjohi <akwanjohi99@gmail.com>
2023-10-13 14:00:54 +0300

    Modify user model and migration
    - Add new fields
    - Implement soft deleting

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone_number')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('verification_code')->nullable()->unique();
+            $table->string('reset_code')->nullable()->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
