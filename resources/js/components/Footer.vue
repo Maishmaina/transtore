9a82b496d278e05be8bda5013b2b2a597db6b578
Alvin Wanjohi <akwanjohi99@gmail.com>
2023-10-16 12:40:11 +0300

    Modify padding and margin on the footer component

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="kt_app_footer" class="app-footer d-flex flex-column flex-md-row flex-center flex-md-stack py-3">
+    <div id="kt_app_footer" class="app-footer d-flex flex-column flex-md-row flex-center flex-md-stack my-4 py-4">
         <div class="text-dark order-2 order-md-1">
             <span class="text-muted fw-semibold me-1">&copy; {{ year }}</span>
             <span class="text-gray-800">Transtores. All Rights Reserved</span>
