10451aa70e2e56243092607e65a165fbc91536e9
Alvin Wanjohi <akwanjohi99@gmail.com>
2023-10-16 12:40:42 +0300

    Modify user controller
    - Sort response from index method by latest

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return UserResource::collection(User::paginate());
+        return UserResource::collection(User::latest()->paginate());
     }
 
     /**
