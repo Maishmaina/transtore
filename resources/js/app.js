f946b2b0ce931cf21345dc2128109b998e3fb1e5
Alvin Wanjohi <akwanjohi99@gmail.com>
2023-10-17 11:44:15 +0300

    Create spinner component and register it globally

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import { createPinia } from "pinia";
 import piniaPluginPersistedState from "pinia-plugin-persistedstate";
 import Vue3Toasity, { toast } from "vue3-toastify";
 import "vue3-toastify/dist/index.css";
+import Spinner from "./components/Spinner.vue";
 
 import App from "./App.vue";
 import router from "./router";
@@ -11,6 +12,8 @@ import router from "./router";
 const app = createApp(App);
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedState);
+
+app.component("Spinner", Spinner);
 
 app.use(router);
 app.use(pinia);
---
+++ resources/js/components/Spinner.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="card">
+        <div class="text-center">
+            <span class="spinner-border spinner-border-lg my-14 p-8 text-primary"></span>
+        </div>
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
