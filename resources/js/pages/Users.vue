aff6548973caa2f68265933b415f18c95a923058
Alvin Wanjohi <akwanjohi99@gmail.com>
2023-10-17 11:54:28 +0300

    Modify users component
    - Implement filtering
    - Implement loader

--- resources/js/pages/Users.vue
+++ resources/js/pages/Users.vue
@@ -1,16 +1,20 @@
 <template>
-    <div class="card">
+    <div class="card" v-if="!processing">
         <div class="card-header border-0 pt-6">
             <div class="card-title">
                 <div class="d-flex align-items-center position-relative my-1">
                     <i class="ki-outline ki-magnifier fs-3 position-absolute ms-5"></i>
-                    <input type="text" class="form-control form-control-solid w-250px ps-12" placeholder="Search" />
+                    <input type="text" class="form-control form-control-solid w-250px ps-12" placeholder="Search" v-model="search"/>
                 </div>
             </div>
             <div class="card-toolbar">
                 <div class="d-flex justify-content-end">
-                    <button type="button" class="btn btn-light-primary me-3" data-bs-toggle="modal" data-bs-target="#filter-modal">
+                    <button type="button" class="btn btn-light-danger me-3" @click="clearFilters" v-if="filter">
                         <i class="ki-outline ki-filter fs-2"></i>
+                        Clear Filters
+                    </button>
+                    <button type="button" class="btn btn-light-primary me-3" data-bs-toggle="modal" data-bs-target="#filter-modal">
+                        <i class="ki-outline ki-filter-search fs-2"></i>
                         Filter
                     </button>
                     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#add-modal">Add Customer</button>
@@ -32,7 +36,7 @@
                         </tr>
                     </thead>
                     <tbody class="fw-semibold text-gray-600">
-                        <tr v-for="user in users.data">
+                        <tr v-for="user in users.data" v-if="dataAvailable">
                             <td>{{ user.first_name }}</td>
                             <td>{{ user.last_name }}</td>
                             <td>{{ user.phone_number }}</td>
@@ -51,9 +55,12 @@
                                 </button>
                             </td>
                         </tr>
+                        <tr v-else>
+                            <td colspan="7" class="text-center">No data available</td>
+                        </tr>
                     </tbody>
                 </table>
-                <div class="mt-3 d-flex justify-content-between align-items-center" v-if="users.data">
+                <div class="mt-3 d-flex justify-content-between align-items-center" v-if="dataAvailable">
                     <div>{{ `Showing ${users.meta.from ?? 0} to ${users.meta.to ?? 0} of ${users.meta.total} entries` }}</div>
                     <nav>
                         <Pagination :data="users" @pagination-change-page="fetchUsers" :limit="5" />
@@ -62,6 +69,8 @@
             </div>
         </div>
     </div>
+
+    <Spinner v-else />
 
     <div class="modal fade" tabindex="-1" id="add-modal">
         <div class="modal-dialog modal-dialog-scrollable">
@@ -166,12 +175,72 @@
                 </div>
 
                 <div class="modal-body">
-                    <p>Modal body text goes here.</p>
+                    <div class="form-group">
+                        <label for="filter-first-name" class="form-label">First Name</label>
+                        <input type="text" 
+                            class="form-control form-control-solid" 
+                            id="filter-first-name" 
+                            v-model="filters.first_name" 
+                            :readonly="processing"
+                        />
+                    </div>
+                    <div class="form-group">
+                        <label for="filter-last-name" class="form-label">Last Name</label>
+                        <input type="text" 
+                            class="form-control form-control-solid" 
+                            id="filter-last-name" 
+                            v-model="filters.last_name" 
+                            :readonly="processing"
+                        />
+                    </div>
+                    <div class="form-group">
+                        <label for="filter-phone-number" class="form-label">Phone No.</label>
+                        <input type="text" 
+                            class="form-control form-control-solid" 
+                            id="filter-phone-number" 
+                            v-model="filters.phone_number" 
+                            :readonly="processing"
+                        />
+                    </div>
+                    <div class="form-group">
+                        <label for="filter-email" class="form-label">Email</label>
+                        <input type="email" 
+                            class="form-control form-control-solid" 
+                            id="filter-email" 
+                            v-model="filters.email" 
+                            :readonly="processing"
+                        />
+                    </div>
+                    <div class="form-group">
+                        <label for="filter-status" class="form-label">Status</label>
+                        <select class="form-select form-select-solid" id="filter-status" v-model="filters.status" :readonly="processing">
+                            <option value="">Select status...</option>
+                            <option value="enabled">Enabled</option>
+                            <option value="disabled">Disabled</option>
+                        </select>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <label for="filter-from-date" class="form-label">From Date</label>
+                            <input type="date" id="filter-from-date" class="form-control form-control-solid" v-model="filters.from_date" :readonly="processing">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="filter-to-date" class="form-label">To Date</label>
+                            <input type="date" id="filter-to-date" class="form-control form-control-solid" v-model="filters.to_date" :readonly="processing">
+                        </div>
+                    </div>
                 </div>
 
                 <div class="modal-footer">
                     <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Submit</button>
+                    <button type="button" class="btn btn-primary" @click="filterUsers">
+                        <span class="indicator-label" v-if="!processing">
+                            Apply Filters
+                        </span>
+                        <span class="indicator-progress d-block" v-else>
+                            Please wait... <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                        </span>
+                    </button>
                 </div>
             </div>
         </div>
@@ -180,10 +249,11 @@
 
 <script setup>
 import moment from 'moment'
-import { onMounted, ref } from 'vue'
-import { useAuthStore } from '@/stores/authStore.js'
-import { toast } from 'vue3-toastify';
 import Swal from 'sweetalert2'
+import throttle from 'lodash/throttle'
+import { toast } from 'vue3-toastify';
+import { computed, onMounted, ref, watch } from 'vue'
+import { useAuthStore } from '@/stores/authStore.js'
 import { Bootstrap5Pagination as Pagination } from 'laravel-vue-pagination';
 
 const { token } = useAuthStore()
@@ -194,18 +264,51 @@ const config = {
     }
 }
 
+const processing = ref(true)
+
 const users = ref({})
+
+const dataAvailable = computed(() => {
+    return Boolean(users.value.data?.length)
+})
+
+const search = ref('')
+
+const filters = ref({
+    first_name: '',
+    last_name: '',
+    phone_number: '',
+    email: '',
+    status: '',
+    from_date: '',
+    to_date: ''
+})
 
 const fetchUsers = async (page = 1) => {
     let response = null
     try {
-        response = await axios.get(`users?page=${page}`, config)
+        response = await axios.get(`users`, {
+            ...config,
+            
+            params: {
+                page,
+                search: search.value,
+                first_name: filters.value.first_name,
+                last_name: filters.value.last_name,
+                phone_number: filters.value.phone_number,
+                email: filters.value.email,
+                status: filters.value.status,
+                from_date: filters.value.from_date,
+                to_date: filters.value.to_date,
+            }
+        })
     } catch (error) {
         response = error.response
     }
     
     if (response.status == 200) {
         users.value = response.data
+        processing.value = false
     } else {
         toast.error("Error fetching customers list")
     }
@@ -216,9 +319,7 @@ onMounted(() => {
     fetchUsers()
 })
 
-// TABLE ACTIONS
 const errors = ref({})
-const processing = ref(false)
 
 const form = ref({
     first_name: '',
@@ -244,21 +345,59 @@ const submitForm = async () => {
     try {
         response = await axios.post('users', form.value, config)
     } catch (error) {
-        toast.error("Error adding customer")
         response = error.response
-        errors.value = response.data.errors
     }
     
     if (response.status == 201) {
-        fetchUsers()
-        $('#add-modal .btn-sm').click()
-        clearForm()
         toast.success("Customer added successfully")
+        clearForm()
+        $('#add-modal .btn-sm').click()
+        fetchUsers()
+    } else if (response.status == 422) {
+        toast.error("Error adding customer")
+        errors.value = response.data.errors
+
+        processing.value = false
     }
-
-    processing.value = false
-
 }
+
+const filter = ref(false)
+
+const filterUsers = async () => {
+    $('#filter-modal .btn-sm').click()
+    
+    processing.value = true
+    
+    await fetchUsers()
+
+    filter.value = true
+}
+
+watch(() => filters.value.from_date, () => {
+    if (!filters.value.to_date) {
+        filters.value.to_date = moment().format('YYYY-MM-D')
+    }
+})
+
+const clearFilters = async () => {
+    processing.value = true
+    
+    filters.value.first_name = ''
+    filters.value.last_name = ''
+    filters.value.phone_number = ''
+    filters.value.email = ''
+    filters.value.status = ''
+    filters.value.from_date = ''
+    filters.value.to_date = ''
+
+    await fetchUsers()
+
+    filter.value = false
+}
+
+watch(() => search.value, throttle((value) => {
+    fetchUsers()
+}, 600))
 
 const deleteItem = (id) => {
     Swal.fire({
@@ -280,17 +419,20 @@ const deleteItem = (id) => {
             }
 
             if (response.status = 200) {
-                fetchUsers()
-
                 Swal.fire(
                     'Deleted!',
                     'Customer has been deleted.',
                     'success'
                 )
+                processing.value = true
+
+                fetchUsers()
+
             }
         }
     })
 }
+
 </script>
 
 <style scoped>
