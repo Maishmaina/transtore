c26efd26281fe3e51aadbab8ed1db039bf8ad1c1
Alvin Wanjohi <akwanjohi99@gmail.com>
2023-10-17 11:51:23 +0300

    Modify user model
    - Add local scopes

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use Illuminate\Support\Carbon;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -62,5 +63,54 @@ class User extends Authenticatable
         return Attribute::make(
             get: fn () => "$this->first_name $this->last_name"
         );
+    }
+
+    public function scopeFirstName($query, $keyword)
+    {
+        return $query->where('first_name', 'like', '%' . $keyword . '%');
+    }
+
+    public function scopeLastName($query, $keyword)
+    {
+        return $query->where('last_name', 'like', '%' . $keyword . '%');
+    }
+
+    public function scopePhoneNumber($query, $keyword)
+    {
+        return $query->where('phone_number', 'like', '%' . $keyword . '%');
+    }
+
+    public function scopeEmail($query, $keyword)
+    {
+        return $query->where('email', 'like', '%' . $keyword . '%');
+    }
+
+    public function scopeSearch($query, $keyword)
+    {
+        return $query->where('first_name', 'like', '%' . $keyword . '%')
+            ->orWhere('last_name', 'like', '%' . $keyword . '%')
+            ->orWhere('phone_number', 'like', '%' . $keyword . '%')
+            ->orWhere('email', 'like', '%' . $keyword . '%');
+    }
+
+    public function scopeStatus($query, $keyword)
+    {
+        return $query->when($keyword == 'enabled', function ($query) {
+            $query->where('enabled', true);
+        })
+            ->when($keyword == 'disabled', function ($query) {
+                $query->where('enabled', false);
+            });
+    }
+
+    public function scopeDate($query, $from, $to)
+    {
+        if ($from && $to) {
+            return $query->whereBetween('created_at', [Carbon::parse($from), Carbon::parse($to)->addDay()]);
+        } elseif (!$from && $to) {
+            return $query->whereDate('created_at', '<=', Carbon::parse($to)->addDay());
+        } else {
+            return $query;
+        }
     }
 }
