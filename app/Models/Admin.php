c95d821337bbe0e5bdf154c4e53c1114aae22c87
Alvin Wanjohi <akwanjohi99@gmail.com>
2023-10-18 10:20:44 +0300

    Implement local scopes on admin model

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -62,4 +62,57 @@ class Admin extends Authenticatable
             get: fn () => "$this->first_name $this->last_name"
         );
     }
+
+    public function scopeFirstName($query, $keyword)
+    {
+        return $query->where('first_name', 'like', '%' . $keyword . '%');
+    }
+
+    public function scopeLastName($query, $keyword)
+    {
+        return $query->where('last_name', 'like', '%' . $keyword . '%');
+    }
+
+    public function scopePhoneNumber($query, $keyword)
+    {
+        if ($keyword) {
+            return $query->where('phone_number', 'like', '%' . $keyword . '%');
+        } else {
+            return $query;
+        }
+    }
+
+    public function scopeEmail($query, $keyword)
+    {
+        return $query->where('email', 'like', '%' . $keyword . '%');
+    }
+
+    public function scopeSearch($query, $keyword)
+    {
+        return $query->where('first_name', 'like', '%' . $keyword . '%')
+            ->orWhere('last_name', 'like', '%' . $keyword . '%')
+            ->orWhere('phone_number', 'like', '%' . $keyword . '%')
+            ->orWhere('email', 'like', '%' . $keyword . '%');
+    }
+
+    public function scopeStatus($query, $keyword)
+    {
+        return $query->when($keyword == 'enabled', function ($query) {
+            $query->where('enabled', true);
+        })
+            ->when($keyword == 'disabled', function ($query) {
+                $query->where('enabled', false);
+            });
+    }
+
+    public function scopeDate($query, $from, $to)
+    {
+        if ($from && $to) {
+            return $query->whereBetween('created_at', [Carbon::parse($from), Carbon::parse($to)->addDay()]);
+        } elseif (!$from && $to) {
+            return $query->whereDate('created_at', '<=', Carbon::parse($to)->addDay());
+        } else {
+            return $query;
+        }
+    }
 }
