2cc3986c4bc6a4b2d69be402290853c57f6ad0ae
Alvin Wanjohi <akwanjohi99@gmail.com>
2023-10-18 15:25:35 +0300

    Create reusable nav-link component

---
+++ resources/js/components/NavLink.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="menu-item me-0 me-lg-2">
+        <router-link :to="route" class="menu-link">
+            <span :class="iconClass ? 'menu-icon' : 'menu-bullet' ">
+                <i :class="iconClass ?? 'bullet bullet-dot'"></i>
+            </span>
+            <span class="menu-title">{{ title }}</span>
+        </router-link>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    title: String,
+    route: Object,
+    iconClass: String
+})
+</script>
+
+<style scoped></style>
