c0bc6dea89768cbb5d5929b357611e9a3b10d9cd
Alvin Wanjohi <akwanjohi99@gmail.com>
2023-10-27 17:00:25 +0300

    Modify facility model
    - Append coordinates attribute
    - Cast enabled field to boolean

--- app/Models/Facility.php
+++ app/Models/Facility.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -19,9 +20,17 @@ class Facility extends Model
         'access_period',
     ];
 
+    protected $appends = [
+        'coordinates'
+    ];
+
+    protected $casts = [
+        'enabled' => 'boolean'
+    ];
+
     public function owner()
     {
-        return $this->belongsTo(FacilityOwner::class);
+        return $this->belongsTo(FacilityOwner::class, 'facility_owner_id');
     }
 
     public function storageTypes()
@@ -42,5 +51,12 @@ class Facility extends Model
     public function reviews()
     {
         return $this->hasMany(FacilityReview::class);
+    }
+
+    public function coordinates(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => "$this->location_latitude, $this->location_longitude"
+        );
     }
 }
